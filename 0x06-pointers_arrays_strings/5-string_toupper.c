6d3f05bfbd2247b23329c6710372c750b2f5dec0
Salma <salmaelaadam@gmail.com>
2023-03-29 05:51:26 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - uppercase a lowercase
+ * @s: character to make uppercase
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
